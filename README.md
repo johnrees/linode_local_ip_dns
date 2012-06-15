dec91b27aae25d5d7f7e4d3db162064d8f18afdd
John Rees <john@bitsushi.com>
2012-06-15 12:30:08 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-linode_local_ip_dns
-===================
+# Linode Local IP DNS
+### Awful name. A genuinely useful tool...
+
+I built this out of frustration with testing on various devices and from different networks. When testing on mobiles etc, my development website runs on 127.0.0.1/0.0.0.0/localhost and then navigate to 192.168.0.1 (or whatever my development machine's IP was) from my various devices.
+
+However, whenever I changed network I would be assigned a different DHCP IP so I'd have to use a different IP address each time.
+
+This attempts to simplify the process by grabbing your Local IP and then creating or updating a Linode subdomain to point to it. If you save it as a Rails initializer it will then update the subdomain whenever you start up your server, or if you are like me, restart pow.
+
+### Notes
+
+If you do use [pow](http://pow.cx) you can run `website` on 0.0.0.0 as well as website.dev by copying the ~/.pow/website to ~/.pow/default
---
+++ linode_local_ip_dns.rb
@@ -0,0 +1,90 @@
+#!/usr/bin/env ruby
+
+# http://github.com/johnrees/linode_local_ip_dns
+
+##-----
+# Usage:
+##-----
+
+# Set the Parameters below.
+# KEY will be the subdomain e.g. 'local' would result in 'local.mydomain.com'
+
+# Required Parameters
+LINODE_API_KEY = 'XXXXXXXXXXXX' # https://manager.linode.com/profile/index#apikey
+DOMAIN = 'mydomain.com'
+
+# Optional Parameters
+KEY = 'local'
+RECORD_TYPE = 'a' # could be cname
+TTL = 300
+DEBUG = true
+
+
+##-----
+## Now start the ugly code party!
+##-----
+
+
+def log msg
+  if DEBUG
+    begin
+      Rails.logger.info msg
+    rescue
+      p msg
+    end
+  end
+end
+
+if (defined? Rails and Rails.env.development?) or true
+
+  require 'json'
+  require 'open-uri'
+  require 'socket'
+
+  url = "https://api.linode.com/?api_key=#{LINODE_API_KEY}"
+  IP = UDPSocket.open {|s| s.connect('64.233.187.99', 1); s.addr.last }
+
+  raise 'Local IP not found' unless defined? IP
+
+  domains_list_url = "#{url}&api_action=domain.list"
+  p "Getting Domains List - #{domains_list_url}"
+  result = JSON.parse(open(domains_list_url).read)
+  result['DATA'].each do |domain|
+    if domain['DOMAIN'] == DOMAIN
+      log 'Found Domain'
+      DOMAINID = domain['DOMAINID']
+    end
+  end
+
+  raise "'#{DOMAIN}' not found in your domains list - https://manager.linode.com/dns" unless defined? DOMAINID
+
+  resources_list_url = "#{url}&api_action=domain.resource.list&DomainID=#{DOMAINID}"
+  log "Getting Resources List - #{resources_list_url}"
+  result = JSON.parse(open(resources_list_url).read)
+  result['DATA'].each do |resource|
+    if resource['NAME'] == KEY
+      log 'Found Resource'
+      RESOURCEID = resource['RESOURCEID']
+    end
+  end
+
+  if defined? RESOURCEID
+
+    update_resource_url = "#{url}&api_action=domain.resource.update&DomainID=#{DOMAINID}&ResourceID=#{RESOURCEID}&Target=#{IP}&TTL_sec=#{TTL}"
+    log "Updating #{KEY}.#{DOMAIN} to point to #{IP}"
+    result = JSON.parse(open(update_resource_url).read)
+    if result['ERRORARRAY'].empty?
+      log "Updated Resource, please allow up to #{15 + TTL/60} minutes for changes to take effect"
+    end
+
+  else
+
+    create_resource_url = "#{url}&api_action=domain.resource.create&DomainID=#{DOMAINID}&Target=#{IP}&Type=a&Name=#{KEY}&TTL_sec=#{TTL}"
+    log "Creating #{KEY}.#{DOMAIN}, to point to #{IP}"
+    result = JSON.parse(open(create_resource_url).read)
+    if result['ERRORARRAY'].empty?
+      log "Created Resource, please allow up to #{15 + TTL/60} minutes for changes to take effect"
+    end
+  end
+
+end
